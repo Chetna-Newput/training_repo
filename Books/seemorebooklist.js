cda8ebe4609fe88adddafd8e170250ad5fe26d87
deepakpatidar <deepak@newput.com>
2016-02-16 17:47:05 +0530

    Infinite scroll functionality added

--- Books/seemorebooklist.js
+++ Books/seemorebooklist.js
@@ -1,3 +1,4 @@
+var limit=2,pg,strng;
 var jsonOb = {};
 $(document).ready( function() {
    $('a').click(function(){
@@ -5,36 +6,71 @@ $(document).ready( function() {
       $(this).addClass("actve");
    });
 
- $('body').on("click", "button", function(){
+ var modal = document.getElementById('myModal');
+
+ $(".close").click(function() {
+     $("#myModal").hide();
+  });
+ 
+ $(window).click(function(event) {
+    if (event.target == modal) {
+       $("#myModal").hide();
+    }
+}  );
+  
+  $('body').on("click", "button", function(){
+   
       var bookid = this.getAttribute("data-bookid");
      for(i=0;i<jsonOb.Books.length;i++) 
      { if(bookid==jsonOb.Books[i].ID)
-        {  console.log(jsonOb.Books[i].Title);
-             console.log(jsonOb.Books[i].Image);
-             console.log(jsonOb.Books[i].Description);
-             console.log(jsonOb.Books[i].isbn);
+        {  
+         $("#titleShow").text(jsonOb.Books[i].Title); 
+         jQuery("#modalImg").attr('src',jsonOb.Books[i].Image );
+         $("#modalDesp").text(jsonOb.Books[i].Description); 
+         $("#modalIsbn").text(jsonOb.Books[i].isbn); 
+          $("#myModal").show();
+           
+         
         }
      
-          }
+      }
    
-    console.log(bookid);
+    
    });
+  
+  $(window).scroll(function() {
+   if($(document).height() <= ($(window).scrollTop()+$(window).height() ))  {
+        change(strng,pg)
+;   }
+});
+  
  });
 
 
-function change(str) {
+function change(str, page) {
+  if(page==1)
+   {jsonOb={};
+     }
+   strng=str; pg=page;
   var arr=[],i,cell="",cells=["","","",""],rem;
-    
+   
+   
    str="http://it-ebooks-api.info/v1/search/".concat(str);
+   str=str.concat("/page/"+page);
+  console.log(str);
+  pg++;
    $.getJSON(str,function(data, status) {
      jsonOb=data;
-     
+     limit=jsonOb.Total; 
+     limit=Math.ceil((limit/10));
+     console.log(pg)
+     if(limit>=pg)
+     {
      if(jsonOb.hasOwnProperty('Books')) 
        { 
          for(i=0;i<jsonOb.Books.length;i++)
-           {  cell="<div class=\"grid\">" + 
+           {  cell="<div id=\"addMargin\" class=\"grid\">" + 
                      "<image src=\""+jsonOb.Books[i].Image+"\"id=\"imgId\" class=\"img-responsive img-thumbnail\">" + 
-                     "<p>ISBN :"+jsonOb.Books[i].isbn+"</p>" + 
                      "<p>Book Title :"+jsonOb.Books[i].Title+"</p>" + 
                      "<p>"+jsonOb.Books[i].Description+"</p>" + 
                      "<button  data-bookid=\""+jsonOb.Books[i].ID+"\">See More</button></div>";
@@ -46,8 +82,13 @@ function change(str) {
             }
          }
      for(i=0;i<4;i++)
-          document.getElementById("cn"+i).innerHTML=cells[i];
-   } 
+     {  if(pg==2)
+           document.getElementById("cn"+i).innerHTML=cells[i];
+         else
+           $("#cn"+i).append(cells[i]);
+       } 
+     }
+     } 
    );
 };
 
